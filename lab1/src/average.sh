c8a8793daaea199bdf0ca2eba38a7a8d742f680e
Shipkov Nikita <nikitashipkov@gmail.com>
2021-02-24 23:31:27 +0300

    lab 1

---
+++ lab1/src/average.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+i=0
+sum=0
+for an_arg in "$@" ; do
+  sum=$(($sum + ${an_arg}))
+  i=$(($i+1))
+done
+expr $sum / $i
+echo $i
---
+++ lab1/src/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pwd
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+echo $PATH
