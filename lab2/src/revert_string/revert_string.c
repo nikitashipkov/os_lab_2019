4f2e5ccb469d43ecf5e711ad77306654dad37dd4
Shipkov Nikita <nikitashipkov@gmail.com>
2021-03-24 22:36:21 +0300

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,20 @@
+
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int i,j,l;
+     char t;
+     l=strlen(str);
+     i=0;
+     j=l-1;
+     while (i<j)
+     {
+        t=str[i];
+        str[i]=str[j];
+        str[j]=t;
+        i++;j--;
+      }
 }
 
